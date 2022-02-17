d909a8ee8669091a7a97216b175937f4dd5a8709
jiji-hoon96 <94469974+jiji-hoon96@users.noreply.github.com>
2022-02-18 02:20:25 +0900

    1-2 Express Middleware 연습, Morgan 설치

--- src/server.js
+++ src/server.js
@@ -1,3 +1,18 @@
 import express from "express";
+import morgan from "morgan";
 
-console.log("hi");
+const app = express();
+const logger = morgan("dev");
+
+const localHost = 4000;
+
+const home = (req, res) => {
+  return res.send(`여기는 localHost=${localHost}의 Server 입니다`);
+};
+
+app.use(logger);
+app.get("/", home);
+
+app.listen(localHost, () =>
+  console.log(`Server Listening on port ${localHost}`)
+);
