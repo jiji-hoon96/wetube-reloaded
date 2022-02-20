713a349e1189b1ac74486eae000b58b66eacb551
jiji-hoon96 <94469974+jiji-hoon96@users.noreply.github.com>
2022-02-20 16:16:28 +0900

    3-1 "가짜 DB를 통해 Edit 변경"
    가짜 DB를 만들어서 Edit을 변경

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,38 +1,51 @@
+let videos = [
+  {
+    title: "첫번째 게시물",
+    rating: 5,
+    comments: 2,
+    createdAt: "2 minutes age",
+    views: 50,
+    id: 1,
+  },
+  {
+    title: "두번째 게시물",
+    rating: 1,
+    comments: 223,
+    createdAt: "10 minutes age",
+    views: 523440,
+    id: 2,
+  },
+  {
+    title: "세번째 게시물",
+    rating: 2,
+    comments: 112,
+    createdAt: "1 minutes age",
+    views: 42,
+    id: 3,
+  },
+];
 export const home = (req, res) => {
-  const videos = [
-    {
-      title: "첫번째 게시물",
-      rating: 5,
-      comments: 2,
-      createdAt: "2 minutes age",
-      views: 50,
-      id: 1,
-    },
-    {
-      title: "두번째 게시물",
-      rating: 1,
-      comments: 223,
-      createdAt: "10 minutes age",
-      views: 523440,
-      id: 2,
-    },
-    {
-      title: "세번째 게시물",
-      rating: 2,
-      comments: 112,
-      createdAt: "1 minutes age",
-      views: 42,
-      id: 3,
-    },
-  ];
   return res.render("home", { pageTitle: "home", videos });
 };
 
-export const watch = (req, res) => res.render("watch", { pageTitle: "watch" });
+export const watch = (req, res) => {
+  const { id } = req.params;
+  const video = videos[id - 1];
+  return res.render("watch", { pageTitle: `Watching : ${video.title}`, video });
+};
 
-export const upload = (req, res) =>
-  res.render("upload", { pageTitle: "upload" });
-export const edit = (req, res) => res.render("edit");
+export const getEdit = (req, res) => {
+  const { id } = req.params;
+  const video = videos[id - 1];
+  return res.render("edit", { pageTitle: `Edit : ${video.title}`, video });
+};
+export const postEdit = (req, res) => {
+  const { id } = req.params;
+  const { title } = req.body;
+  videos[id - 1].title = title;
+  return res.redirect(`/videos/${id}`);
+};
+
 export const remove = (req, res) => {
   res.send("여기는 비디오를 지우는 페이지입니다");
 };
--- src/server.js
+++ src/server.js
@@ -6,11 +6,12 @@ import videoRouter from "./routers/videoRouter";
 
 const app = express();
 const logger = morgan("dev");
-app.use(logger);
 const localHost = 4000;
 
 app.set("view engine", "pug");
 app.set("views", process.cwd() + "/src/views");
+app.use(logger);
+app.use(express.urlencoded({ extended: true }));
 app.use("/", globalRouter);
 app.use("/videos", videoRouter);
 app.use("/users", userRouter);
