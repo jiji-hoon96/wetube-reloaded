307d77896bdfcbc7a80b8d481acdd1d730e146c4
jiji-hoon96 <94469974+jiji-hoon96@users.noreply.github.com>
2022-02-21 01:21:12 +0900

    3-6 "findByIdAndUpdate, exists 사용"
    1. findByIdAndUpdate를 이용해서 getEdit에서 가져온 값을 postEdit에서 고쳐줌
    2. exists를 사용해서 가져온 id와 Video의 _id가 일치해서 비디오가 있는지 확인하고 true/false값을 출력해 봤음

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -7,18 +7,36 @@ export const home = async (req, res) => {
 
 export const watch = async (req, res) => {
   const { id } = req.params;
-  const video = await Video.findById(id);
+  const video = await Video.findById(id).exec();
+  if (!video) {
+    return res.render("404", { pageTitle: "주소가 잘못된 페이지입니다" });
+  }
   return res.render("watch", { pageTitle: video.title, video });
 };
 
-export const getEdit = (req, res) => {
+export const getEdit = async (req, res) => {
   const { id } = req.params;
-  return res.render("edit", { pageTitle: "Editing" });
+  const video = await Video.findById(id).exec();
+  if (!video) {
+    return res.render("404", { pageTitle: "주소가 잘못된 페이지입니다" });
+  }
+  return res.render("edit", { pageTitle: `Edit : ${video.title}`, video });
 };
-export const postEdit = (req, res) => {
+export const postEdit = async (req, res) => {
   const { id } = req.params;
-  const { title } = req.body;
-  return res.redirect(`/videos/${id}`);
+  const { title, description, hashtags } = req.body;
+  const video = await Video.exists({ _id: id });
+  if (!video) {
+    return res.render("404", { pageTitle: "주소가 잘못된 페이지입니다" });
+  }
+  await Video.findByIdAndUpdate(id, {
+    title,
+    description,
+    hashtags: hashtags
+      .split(",")
+      .map((word) => (word.startsWith("#") ? word : `#${word}`)),
+  });
+  return res.redirect("/");
 };
 
 export const getUpload = (req, res) => {
@@ -31,7 +49,9 @@ export const postUpload = async (req, res) => {
     await Video.create({
       title,
       description,
-      hashtags: hashtags.split(",").map((word) => `#${word}`),
+      hashtags: hashtags
+        .split(",")
+        .map((word) => (word.startsWith("#") ? word : `#${word}`)),
     });
     return res.redirect("/");
   } catch (error) {
