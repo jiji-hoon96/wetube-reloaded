6d2a4da2b69274ed43ab8b40a2ecb4d896873c01
jiji-hoon96 <94469974+jiji-hoon96@users.noreply.github.com>
2022-02-21 22:39:27 +0900

    4-2 "bcrypt를 이용한 로그인페이지 구현"
    1. bcrypt를 이용해서 hash함수화된 pw와 입력된 pw를 비교해 로그인 성공하는 페이지 만듬
    compare()

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,4 +1,5 @@
 import User from "../models/User";
+import bcrypt from "bcrypt";
 
 export const getJoin = (req, res) => res.render("join", { pageTitle: "join" });
 export const postJoin = async (req, res) => {
@@ -18,18 +19,48 @@ export const postJoin = async (req, res) => {
       errorMessage: "email 또는 username이 이미 있는 값입니다",
     });
   }
-  await User.create({
-    name,
-    email,
-    username,
-    password,
-    location,
-  });
-  return res.redirect("/login");
+  try {
+    await User.create({
+      name,
+      email,
+      username,
+      password,
+      location,
+    });
+    return res.redirect("/login");
+  } catch (error) {
+    {
+      return res.status(400).render("join", {
+        pageTitle,
+        errorMessage: error._message,
+      });
+    }
+  }
 };
 export const edit = (req, res) => res.send("Edit User");
 export const remove = (req, res) => res.send("Remove User");
-export const login = (req, res) => res.render("login", { pageTitle: "Login" });
+export const getLogin = (req, res) =>
+  res.render("login", { pageTitle: "Login" });
+export const postLogin = async (req, res) => {
+  const { username, password } = req.body;
+  const pageTitle = "Login";
+  const user = await User.findOne({ username }); //username과 동일한 이름을 User.js에서 찾아서 user에 대입
+  if (!user) {
+    return res.status(400).render("login", {
+      pageTitle,
+      errorMessage: "없는 username 입니다",
+    });
+  }
+  const ok = await bcrypt.compare(password, user.password);
+  if (!ok) {
+    return res.status(400).render("login", {
+      pageTitle,
+      errorMessage: "잘못된 Password 입니다",
+    });
+  }
+  console.log("로그인성공했다 짜짜짜짜아안!!!!!!!");
+  return res.redirect("/");
+};
 export const watch = (req, res) =>
   res.send("여기는 유저를 프로필을 보는 공간입니다");
 
