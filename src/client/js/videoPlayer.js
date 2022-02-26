c243725eeda7c2b760939f3141c4213f2aa78786
jiji-hoon96 <94469974+jiji-hoon96@users.noreply.github.com>
2022-02-26 23:17:28 +0900

    8-5 "Video Controller SCSS마무리, 수정"
    1. 비디오 클릭,스페이스바 event 구현
    2. header scss 수정

--- src/client/js/videoPlayer.js
+++ src/client/js/videoPlayer.js
@@ -1,17 +1,21 @@
 const video = document.querySelector("video");
 const videoContainer = document.getElementById("videoContainer");
 const playBtn = document.getElementById("play");
+const playBtnIcon = playBtn.querySelector("i");
 const muteBtn = document.getElementById("mute");
-const currentTime = document.getElementById("currentTime");
+const muteBtnIcon = muteBtn.querySelector("i");
+const currentTime = document.getElementById("currenTime");
 const totalTime = document.getElementById("totalTime");
 const volumeRange = document.getElementById("volume");
 const timeline = document.getElementById("timeline");
-const fullscreenBtn = document.getElementById("fullScreen");
+const fullScreenBtn = document.getElementById("fullScreen");
+const fullScreenIcon = fullScreenBtn.querySelector("i");
 const videoControls = document.getElementById("videoControls");
 
 let volumeValue = 0.5;
 let controlsTimeout = null;
 let controlsMovementTimeout = null;
+let clickOption = true;
 video.volume = volumeValue;
 
 const handlePlayClick = (e) => {
@@ -20,7 +24,7 @@ const handlePlayClick = (e) => {
   } else {
     video.pause();
   }
-  playBtn.innerText = video.paused ? "Play" : "Pause";
+  playBtnIcon.classList = video.paused ? "fas fa-play" : "fas fa-pause";
 };
 
 const handleMute = (e) => {
@@ -33,7 +37,10 @@ const handleMute = (e) => {
     video.volume = 0.3;
     volumeValue = 0.3;
   }
-  muteBtn.innerText = video.muted ? "UnMute" : "Mute";
+  muteBtnIcon.classList = video.muted
+    ? "fas fa-volume-mute"
+    : "fas fa-volume-up";
+
   volumeRange.value = video.muted ? 0 : volumeValue;
 };
 
@@ -43,14 +50,14 @@ const handleVolumeChange = (event) => {
   } = event;
   if (video.muted) {
     video.muted = false;
-    muteBtn.innerText = "Mute";
+    muteBtnIcon.classList = "fas fa-volume-up";
   }
   volumeValue = Number(value);
   video.volume = value;
 
   if (volumeValue === 0) {
     video.muted = true;
-    muteBtn.innerText = "UnMute";
+    muteBtnIcon.classList = "fas fa-volume-mute";
   }
 };
 
@@ -78,10 +85,10 @@ const handleFullscreen = () => {
   const fullscreen = document.fullscreenElement;
   if (fullscreen) {
     document.exitFullscreen();
-    fullscreenBtn.innerText = "Enter Full Screen";
+    fullScreenIcon.classList = "fas fa-expand";
   } else {
     videoContainer.requestFullscreen();
-    fullscreenBtn.innerText = "Exit Full Screen";
+    fullScreenIcon.classList = "fas fa-compress";
   }
 };
 
@@ -104,15 +111,34 @@ const handleMouseLeave = () => {
   controlsTimeout = setTimeout(hideControls, 3000);
 };
 
+const handleClickScreen = () => {
+  if (clickOption) {
+    video.play();
+    clickOption = false;
+  } else {
+    video.pause();
+    clickOption = true;
+  }
+  playBtnIcon.classList = clickOption ? "fas fa-play" : "fas fa-pause";
+};
+
+const handleKeyPress = (event) => {
+  if (event.key === " ") {
+    handleClickScreen();
+  }
+};
+
 playBtn.addEventListener("click", handlePlayClick);
 muteBtn.addEventListener("click", handleMute);
 volumeRange.addEventListener("input", handleVolumeChange);
-video.addEventListener("loadedmetadata", handleLoadedMetadata);
+video.addEventListener("loadeddata", handleLoadedMetadata);
 video.addEventListener("timeupdate", handleTimeUpdate);
 timeline.addEventListener("input", handleTimelineChange);
-fullscreenBtn.addEventListener("click", handleFullscreen);
-video.addEventListener("mousemove", handleMouseMove);
-video.addEventListener("mouseleave", handleMouseLeave);
+fullScreenBtn.addEventListener("click", handleFullscreen);
+videoContainer.addEventListener("mousemove", handleMouseMove);
+videoContainer.addEventListener("mouseleave", handleMouseLeave);
+video.addEventListener("click", handleClickScreen);
+document.addEventListener("keydown", handleKeyPress);
 
 if (video.readyState == 4) {
   handleLoadedMetadata();
